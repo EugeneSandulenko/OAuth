a1ab92eb25cc4f1065f462c960bbf29726d8f1e1
Eugene Sanduelnko <eugene.s@langate.com>
2018-07-01 08:51:51 -0400

    OAuth

---
+++ OAuth/Infrastructure/InMemoryConfig.cs
@@ -0,0 +1,48 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OAuth.Infrastructure
+{
+    public class InMemoryConfig
+    {
+        public static IEnumerable<ApiResource> ApiResources()
+        {
+            return new ApiResource[]
+            {
+                new ApiResource("AothDemo","DemoAuth")
+            };
+        }
+
+        public static IEnumerable<Client> ApiClients()
+        {
+            return new Client[]
+            {
+                new Client
+                {
+                   ClientId = "AothDemo",
+                   ClientSecrets = new [] { new Secret("demo".Sha256()) },
+                   AllowedGrantTypes = GrantTypes .ResourceOwnerPasswordAndClientCredentials,
+                   AllowedScopes = new [] { "AothDemo" }
+                }
+            };
+        }
+
+        public static IEnumerable<TestUser> Users()
+        {
+            return new []
+            {
+               new TestUser
+               {
+                   SubjectId = "123",
+                   Username = "Demo",
+                   Password = "demoPassword"
+               }
+            };
+        }
+
+    }
+}
---
+++ OAuth/Startup.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using OAuth.Infrastructure;
+using System.Linq;
+
+namespace OAuth
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(InMemoryConfig.ApiResources())
+                .AddInMemoryClients(InMemoryConfig.ApiClients())
+                .AddTestUsers(InMemoryConfig.Users().ToList());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactor)
+        {
+            loggerFactor.AddConsole();
+            app.UseIdentityServer();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+        }
+    }
+}
